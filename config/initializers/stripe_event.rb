f47423314db08de9fe3414166948a9e88291f3d3
Christoph Engelhardt <christoph@it-engelhardt.de>
2019-04-25 20:49:49 +0200

    -fix: Fixed the handling of Stripe Connect webhooks for good

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -2,16 +2,15 @@ StripeEvent.configure do |events|
   events.subscribe 'charge.failed' do |event|
     # aborts execution for events that came through Stripe Connect webhooks
     # they are irrelevant to Koudoku
-    break if event.try(:account)
-    
-    stripe_id = event.data.object['customer']
-    subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.charge_failed
+    unless event.try(:account)
+      stripe_id = event.data.object['customer']
+      subscription = ::Subscription.find_by_stripe_id(stripe_id)
+      subscription.charge_failed
+    end
   end
   
   events.subscribe 'invoice.payment_succeeded' do |event|
     unless event.try(:account)
-      
       stripe_id = event.data.object['customer']
       amount = event.data.object['total'].to_f / 100.0
       subscription = ::Subscription.find_by_stripe_id(stripe_id)
@@ -20,18 +19,18 @@ StripeEvent.configure do |events|
   end
   
   events.subscribe 'charge.dispute.created' do |event|
-    break if event.try(:account)
-    
-    stripe_id = event.data.object['customer']
-    subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.charge_disputed
+    unless event.try(:account)
+      stripe_id = event.data.object['customer']
+      subscription = ::Subscription.find_by_stripe_id(stripe_id)
+      subscription.charge_disputed
+    end
   end
   
   events.subscribe 'customer.subscription.deleted' do |event|
-    break if event.try(:account)
-    
-    stripe_id = event.data.object['customer']
-    subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.subscription_owner.try(:cancel)
+    unless event.try(:account)
+      stripe_id = event.data.object['customer']
+      subscription = ::Subscription.find_by_stripe_id(stripe_id)
+      subscription.subscription_owner.try(:cancel)
+    end
   end
 end
